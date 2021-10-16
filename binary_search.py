e551787a9135a1e2eaca84a0939e5eba8ebefa0f
Will Hopkins <me@willhopkins.xyz>
2021-10-16 16:43:11 -0400

    Adds recursive approach to binary search.
    Note the teacher suggests some shortcuts that I don't think are best practices.

--- binary_search.py
+++ binary_search.py
@@ -27,7 +27,31 @@ def binarySearchIterative(input_list, key):
     return False
 
 
+# This function recursively calls itself until it either finds the
+# requested key, or finishes searching.
+# Note to self: this runs in O(log N) time.
+def binarySearchRecursive(input_list, key, low, high):
+    if low > high:
+        return False
+    else:
+        mid = findMidPoint(low, high)
+        print(mid)
+        if input_list[mid] == key:
+            return True
+        elif input_list[mid] > key:
+            high = mid - 1
+            # The teacher suggests passing mid - 1 as a parameter, but I'm uncomfortable
+            # making the change in the call.
+            return binarySearchRecursive(input_list, key, low, high)
+        else:
+            low = mid + 1
+            return binarySearchRecursive(input_list, key, low, high)
+
+
 a_list = [1, 2, 4, 5, 20]
 a_key = 5
+a_low = 0
+a_high = 4
 
 print(binarySearchIterative(a_list, a_key))
+print(binarySearchRecursive(a_list, a_key, a_low, a_high))
